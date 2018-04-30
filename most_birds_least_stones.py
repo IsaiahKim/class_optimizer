06155f272443c75ca6b2e02226d0b59771ae55de
Isaiah Kim <isaiahkim@berkeley.edu>
2018-04-30 02:10:18 -0700

    Formatting, multiple page scans still breaks on some pdfs

--- most_birds_least_stones.py
+++ most_birds_least_stones.py
@@ -1,5 +1,28 @@
 from tabula import read_pdf
+from os import path
+from glob import glob 
 
-div_usa = read_pdf("Diversity-American.pdf")
+class_count = 100
+class_dict = {}
 
-print(div_usa)
+birds = glob(path.join('.',"*.{}".format('pdf')))
+
+def scrape_pdf(req):
+	catalog = read_pdf(req, pages="1")
+	for i in range(2, len(catalog)):
+		tag = catalog['Unnamed: 0'][i] + catalog['Unnamed: 1'][i]
+		if tag not in class_dict:
+			class_dict[tag] = req[:-4]
+		else:
+			class_dict[tag] = class_dict.get(tag) + ", " + req[:-4]
+			
+for bird in birds:
+	scrape_pdf(bird[2:])
+
+listed = 0
+for stone in sorted(class_dict, key=lambda stone:len(class_dict[stone].split()), reverse=True):
+	if listed < class_count:
+		print("Class: " + stone + " | Requirements Met: " + class_dict[stone])
+		listed += 1
+	else:
+		break
